56d3ae9cca884563de81bb8abbf3db537ca635ca
Milad Nekofar <milad@nekofar.com>
2021-09-01 08:30:52 +0430

    chore: update plugin name all over boilerplate files

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Pest\PluginName;
+namespace Pest\Slim;
 
 use Pest\Plugin;
 use PHPUnit\Framework\TestCase;
--- src/Example.php
+++ src/Example.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Pest\PluginName;
+namespace Pest\Slim;
 
 use PHPUnit\Framework\TestCase;
 
