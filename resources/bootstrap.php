c3849ce07c588a5fc97e426470d03012008215df
Milad Nekofar <milad@nekofar.com>
2021-09-15 19:30:47 +0430

    test: improve bootstrap routes and update tests

--- resources/bootstrap.php
+++ resources/bootstrap.php
@@ -2,17 +2,16 @@
 
 declare(strict_types=1);
 
+use DI\Bridge\Slim\Bridge;
 use DI\Container;
-use Fig\Http\Message\StatusCodeInterface as StatusCode;
-use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\MessageInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 
 // Create Container using PHP-DI
 $container = new Container();
 
 // Configure the application via container
-$app = AppFactory::createFromContainer($container);
+$app = Bridge::create($container);
 
 // Add Routing Middleware
 $app->addRoutingMiddleware();
@@ -20,48 +19,41 @@ $app->addRoutingMiddleware();
 // Add Error Middleware
 $app->addErrorMiddleware(false, false, false);
 
-$app->any('/text', function (Request $request, Response $response): Response {
-    $response->getBody()->write('hello, world');
+$app->any('/status/{code}', function (Response $response, int $code): Response {
+    return $response->withStatus($code);
+});
+
+$app->any('/text/{type}', function (Response $response, string $type): Response {
+    if ('plain' === $type) {
+        $response->getBody()->write('hello, world');
+    } elseif ('html' === $type) {
+        $response->getBody()->write('<p>hello, <i>world</i></p>');
+    }
 
     return $response;
 });
 
-$app->any('/json', function (Request $request, Response $response): Response {
-    $response->getBody()->write('{"hello":"world"}');
+$app->any('/json/{type}', function (Response $response, string $type): Response {
+    if ('one' === $type) {
+        $response->getBody()->write('{"hello":"world"}');
+    } elseif ('two' === $type) {
+        $response->getBody()->write('{"foo":"bar","baz":"qux"}');
+    } elseif ('third' === $type) {
+        $response->getBody()->write('[{"foo":"bar","baz":"qux"}]');
+    }
 
     return $response;
 });
 
-$app->get('/token', function (Request $request, Response $response): Response {
-    return $response
-        ->withHeader('Authorization', $request->getHeader('Authorization'))
-        ->withStatus(StatusCode::STATUS_OK);
+$app->get('/head/{type}', function (Request $request, Response $response, string $type): Response {
+    if ('test' === $type) {
+        $response = $response->withHeader('X-Test', $request->getHeader('X-Test'));
+    } elseif ('auth' === $type) {
+        $response = $response->withHeader('Authorization', $request->getHeader('Authorization'));
+    }
+
+    return $response;
 });
 
-$app->get('/header', function (Request $request, Response $response): Response {
-    return $response
-        ->withHeader('X-Test', $request->getHeader('X-Test'))
-        ->withStatus(StatusCode::STATUS_OK);
-});
-
-$app->post('/created', function (Request $request, Response $response): Response {
-    return $response
-        ->withStatus(StatusCode::STATUS_CREATED);
-});
-
-$app->post('/forbidden', function (Request $request, Response $response): Response {
-    return $response
-        ->withStatus(StatusCode::STATUS_FORBIDDEN);
-});
-
-$app->post('/unauthorized', function (Request $request, Response $response): Response {
-    return $response
-        ->withStatus(StatusCode::STATUS_UNAUTHORIZED);
-});
-
-$app->post('/unprocessable', function (Request $request, Response $response): Response {
-    return $response
-        ->withStatus(StatusCode::STATUS_UNPROCESSABLE_ENTITY);
-});
 
 return $app;
